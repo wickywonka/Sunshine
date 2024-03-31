f8dfc869f1296e6928694c82e983d6fc6c323d07
qiin <chou0212@163.com>
2024-03-31 16:32:09 +0800

    fix: update check

--- src_assets/common/assets/web/ResourceCard.vue
+++ src_assets/common/assets/web/ResourceCard.vue
@@ -5,10 +5,10 @@
             <br>
             <p>{{ $t('resource_card.resources_desc') }}</p>
             <div class="card-group p-4 align-items-center">
-                <a class="btn btn-success m-1" href="https://app.lizardbyte.dev" target="_blank">
-                    {{ $t('resource_card.lizardbyte_website') }}</a>
+                <a class="btn btn-success m-1" href="https://docs.qq.com/aio/DSGdQc3htbFJjSFdO" target="_blank">
+                    <i class="fab fa-fw fa-file"></i> 使用教程</a>
                 <a class="btn btn-primary m-1" href="https://qm.qq.com/q/3tWBFVNZ" target="_blank">
-                    <i class="fab fa-fw fa-discord"></i> 加入串流裙</a>
+                    <i class="fab fa-fw fa-qq"></i> 加入串流裙</a>
                 <a class="btn btn-secondary m-1" href="https://github.com/qiin2333/moonlight-android/releases/tag/shortcut" target="_blank">
                     <i class="fab fa-fw fa-github"></i> 威力加强版 Moonlight-Android 下载</a>
             </div>
